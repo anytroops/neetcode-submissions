3d86d6352c3f47587bb7dd14bd17bba60d47fc65
Sean Xue <anytroops12345@gmail.com>
2026-07-22 16:55:57 -0400

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        seen = {}
+        def back(i, rem):
+            if (i, rem) in seen:
+                return seen[(i, rem)]
+            if rem == 0:
+                return 1
+            if rem < 0 or i == len(coins):
+                return 0
+            seen[(i, rem)] = (back(i, rem - coins[i]) + back(i + 1, rem))
+            return seen[(i, rem)]
+        return back(0, amount)
+        
